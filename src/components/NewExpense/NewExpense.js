f017aa7134026235858108aabb07b8dfbc9a788a
이찬호 <124230969+leechan1234@users.noreply.github.com>
2023-11-15 23:24:35 +0900

    props

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,8 +1,9 @@
+import React from "react";
+
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
-  //expenseData 를 보내줄려니까 인자에 이름을 명시 자식 - 부모 컴포넌트
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
@@ -10,6 +11,7 @@ const NewExpense = (props) => {
     };
     props.onAddExpense(expenseData);
   };
+
   return (
     <div className="new-expense">
       <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
